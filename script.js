6d01fb9d432d4cb90e1d1ce0cb56de281620a1f8
Ryan Duran <dogoathena@gmail.com>
2024-01-24 08:57:55 -0800

    1st

---
+++ script.js
@@ -0,0 +1,61 @@
+let num1 = document.getElementById('numone');
+let num2 = document.getElementById('numtwo');
+
+
+
+
+
+
+
+
+function add(num1, num2) {
+    return(num1 + num2);
+}
+
+function sub(num1, num2) {
+    return(num1 - num2);
+}
+
+function mult(num1, num2) {
+    return(num1 * num2);
+}
+
+function div(num1, num2) {
+    return(num1 / num2);
+}
+
+function exp(num1, num2) {
+    return(num1 ^ num2);
+}
+
+function mod(num1, num2) {
+    return(num1 % num2);
+}
+
+function sin(num1) {
+    return(Math.sin(num1));
+}
+
+function cos(num1) {
+    return(Math.cos(num1));
+}
+
+function tan(num1) {
+    return(Math.tan(num1));
+}
+
+function sqroot(num1) {
+    return(Math.sqrt(num1));
+}
+
+function asin(num1) {
+    return(Math.asin(num1));
+}
+
+function acos(num1) {
+    return(Math.acos(num1));
+}
+
+function atan(num1) {
+    return(Math.atan(num1));
+}
