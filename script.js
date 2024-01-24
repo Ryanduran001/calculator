7d88e4305f4127a52e0e24a6fe3900f81b539ebe
Ryan Duran <dogoathena@gmail.com>
2024-01-24 09:50:20 -0800

    2nd

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let num1 = document.getElementById('numone');
-let num2 = document.getElementById('numtwo');
+// let num1 = document.getElementById('numone').value;
+// let num2 = document.getElementById('numtwo').value;
+
+
+// document.getElementById('add').addEventListener("click", addClick)
+// document.getElementById('sub').addEventListener("click", sub(num1, num2))
+// document.getElementById('mult').addEventListener("click", mult(num1, num2))
+// document.getElementById('div').addEventListener("click", div(num1, num2))
+// document.getElementById('exp').addEventListener("click", exp(num1, num2))
+// document.getElementById('mod').addEventListener("click", mod(num1, num2))
+// document.getElementById('sin').addEventListener("click", sin(num1))
+// document.getElementById('cos').addEventListener("click", cos(num1))
+// document.getElementById('tan').addEventListener("click", tan(num1))
+// document.getElementById('sqrt').addEventListener("click", sqroot(num1))
+// document.getElementById('asin').addEventListener("click", asin(num1))
+// document.getElementById('acos').addEventListener("click", acos(num1))
+// document.getElementById('atan').addEventListener("click", atan(num1))
 
 
 
-
-
+function addClick() {
+    // document.write(`<h2>Answer</h2> ${add(num1, num2)}`)
+    var x = document.getElementById("oneinput").value;
+    var l = document.getElementById("twoinput").value;
+    let k = x + l;
+    document.write(`<h2>The Answer Is ${k}<h2>`);
+}
 
 
 
